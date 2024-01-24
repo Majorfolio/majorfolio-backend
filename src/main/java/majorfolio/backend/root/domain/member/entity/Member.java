a0526da92afc5999d898a1f6f002c32e63566f08
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-01-24 10:08:21 +0900

    MF-84, MF-105, MF-106: member, userToken 테이블 수정

--- src/main/java/majorfolio/backend/root/domain/member/entity/Member.java
+++ src/main/java/majorfolio/backend/root/domain/member/entity/Member.java
@@ -1,46 +1,73 @@
 package majorfolio.backend.root.domain.member.entity;
 
+/**
+ * Memeber
+ *
+ * 0.0.1
+ *
+ * 2024.01.23
+ *
+ * Majorfolio
+ */
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity
-@Table(name = "memeber")
-@Getter
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+/**
+ * memeber테이블 생성
+ *
+ * @author 김영록
+ * @version 0.0.1
+ *
+ * member 테이블 수정(01.24)
+ *
+ * @author 김영록
+ * @version 0.0.1
+ *
+ */
 @Builder
-@AllArgsConstructor
+@Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String kakaoId;
-
-    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false)
     private String nickname;
+    private Boolean emailVerified;
+    @ColumnDefault("true")
+    private Boolean personalInformationIsagree;
+    @ColumnDefault("true")
+    private Boolean serviceIsagree;
+    private String status;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-    @Column(nullable = true)
-    private int profilePic;
+    @OneToOne
+    @JoinColumn(name="kakao_id")
+    private UserToken userToken;
 
-    @Column(nullable = false)
-    private boolean emailVerified;
-
-    public static Member of(Long id, String kakaoId, String email, String nickname, int profilePic, Boolean emailVerified) {
+    public static Member of(String email, String nickname, Boolean emailVerified, String status, UserToken userToken){
         return Member.builder()
-                .id(id)
-                .kakaoId(kakaoId)
                 .email(email)
                 .nickname(nickname)
-                .profilePic(profilePic)
                 .emailVerified(emailVerified)
+                .status(status)
+                .userToken(userToken)
                 .build();
     }
-
 }
