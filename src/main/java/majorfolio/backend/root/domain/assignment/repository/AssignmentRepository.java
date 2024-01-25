36c970cdae314a3e3873b356d24486baed2916a0
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-01-25 11:32:25 +0900

    MF-52 : 최근 업로드, 최근 자료를 본 과제 5개 리스트 반환하는 기능을 위한 레포지토리 기능 구현

--- src/main/java/majorfolio/backend/root/domain/assignment/repository/AssignmentRepository.java
+++ src/main/java/majorfolio/backend/root/domain/assignment/repository/AssignmentRepository.java
@@ -2,14 +2,13 @@ package majorfolio.backend.root.domain.assignment.repository;
 
 import majorfolio.backend.root.domain.assignment.entity.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     //각 기능을 하는 Query들을 작성해줘야 함
-    List<Assignment> findTop5ByOrderByCreatedAtDesc();
+    List<Assignment> findTop5ByOrderByCreatedTimeDesc();
 
-    List<Assignment> findTop5ByOrderByLikesDesc();
-
-    List<Assignment> findTop5ByOrderByUpdatedAtDesc();
+    List<Assignment> findTop5ByOrderByViewTimestampDesc();
 }
