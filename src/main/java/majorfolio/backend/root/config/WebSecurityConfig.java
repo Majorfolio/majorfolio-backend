f74fa5027b030499416f32505eb360993236deff
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-02-01 15:01:29 +0900

    Fix: "home/all/**" 에 해당하는 api는 토큰 없어도 가능하도록 권한 수정

--- src/main/java/majorfolio/backend/root/config/WebSecurityConfig.java
+++ src/main/java/majorfolio/backend/root/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig {
                         "/health-check",
                         "/v3/api-docs/**",
                         "/home/banner",
-                        "home/all/univ/**")
+                        "home/all/**")
                 .requestMatchers("/*"); // 인증 처리 하지 않을 케이스
     }
 }
