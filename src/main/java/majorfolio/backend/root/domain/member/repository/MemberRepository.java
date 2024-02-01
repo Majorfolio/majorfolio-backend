7c5682f87793d26f9c924d8b4aee461dd1f770a7
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-02-01 16:10:13 +0900

    MF-114: nickName으로 멤버를 찾는 기능 구현

--- src/main/java/majorfolio/backend/root/domain/member/repository/MemberRepository.java
+++ src/main/java/majorfolio/backend/root/domain/member/repository/MemberRepository.java
@@ -21,4 +21,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsMemberByNickName(String nickname);
+
+    Member findByNickName(String nickName);
 }
