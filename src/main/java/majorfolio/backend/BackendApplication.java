5390ea76be04e15912808cf72d536ffc2012ec2c
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-02-05 21:49:18 +0900

    fix: swagger https cors 오류 해결

--- src/main/java/majorfolio/backend/BackendApplication.java
+++ src/main/java/majorfolio/backend/BackendApplication.java
@@ -1,7 +1,12 @@
 package majorfolio.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://majorfolio-server.shop/", description = "Default Server URL")})
+//@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 public class BackendApplication {
