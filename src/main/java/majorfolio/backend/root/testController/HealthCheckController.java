0eea66dcb815d50e1ad487824f40a65585f275e8
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-02-17 17:26:32 +0900

    MF-322 : test

--- src/main/java/majorfolio/backend/root/testController/HealthCheckController.java
+++ src/main/java/majorfolio/backend/root/testController/HealthCheckController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping
     public String healthCheck(){
-        return "I am alive!";
+        return "I am alive! always...";
     }
 }
